ab7cbc846309b6af74c3f3be265cedbec4ac6623
glorious <dhao2024@outlook.com>
2026-07-12 16:40:25 +0800

    first

---
+++ chapter2/exercises/2.4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e, f, g, h, i, j;
+    float k, l, m, n, o, p, q;
+
+    printf("%d %d %d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h, i, j);
+    printf("%f %f %f %f %f %f %f\n", k, l, m, n, o, p, q);
+
+    return 0;
+}
